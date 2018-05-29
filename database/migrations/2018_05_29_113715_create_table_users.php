438b83e3017ae5a18171e65afa75ca789b217ad0
bsun33 <bsun33@alumni.uwo.ca>
2018-05-29 08:06:50 -0400

    add users migrations

--- database/migrations/2018_05_29_113715_create_table_users.php
+++ database/migrations/2018_05_29_113715_create_table_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTableUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username', 30)->unique();
+            $table->string('email', 60)->unique()->nullable();
+            //$table->string('country_code')->default('+1');
+            $table->string('phone', 50)->unique()->nullable();
+            $table->string('password', 180);
+            $table->text('intro', 180)->nullable();
+            $table->text('avatar_url')->nullable();
+
             $table->timestamps();
         });
     }
